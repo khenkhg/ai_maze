29f67f6641139f55d309652f9b94ce2fd6b0e864
khenkhg <goudjilkhenssa@gmail.com>
2025-03-18 22:42:55 +0100

    Added backend for Render

--- ai.py
+++ ai.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  # To allow cross-origin requests
 from collections import deque
 import heapq
-
+import os
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
@@ -171,4 +171,5 @@ def solve_maze():
     return jsonify({"solution": solution, "explored": explored, "cost": cost})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from environment (default 5000)
+    app.run(host='0.0.0.0', port=port, debug=True)
